bef2f64bf1ee08ab1546d577cd0f31dad7d9a45b
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-04 14:46:07 -0600

    added colorette to add colors to console output & added first switch statement for arguments

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const minimist = require("minimist");
+const { red, blue, yellow } = require("colorette");
 module.exports = () => {
-  console.log("Welcome to the coinPriceCLI");
+  const args = minimist(process.argv.slice(2));
+  const cmd = args._[0];
+
+  console.log(yellow("Welcome to the coinPriceCLI"));
+
+  switch (cmd) {
+    case "Bitcoin":
+      require("./cmd/Bitcoin")(args);
+      break;
+    default:
+      console.error(red(`"${cmd}" is not a valid command!`));
+      break;
+  }
 };
