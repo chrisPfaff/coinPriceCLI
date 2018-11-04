bfc7a970cf16a8896b4c3961f88936c819a44254
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-04 17:31:21 -0600

    changed index.js to allow for help command

--- cmd/Bitcoin.js
+++
@@ -1,5 +0,0 @@
-const { green } = require("colorette");
-
-module.exports = args => {
-  console.log(green("Bitcoin is legit yo"));
-};
--- index.js
+++ index.js
@@ -2,14 +2,34 @@ const minimist = require("minimist");
 const { red, blue, yellow } = require("colorette");
 module.exports = () => {
   const args = minimist(process.argv.slice(2));
-  const cmd = args._[0];
 
-  console.log(yellow("Welcome to the coinPriceCLI"));
+  let cmd = args._[0] || "help";
+
+  if (args.version || args.v) {
+    cmd = "version";
+  }
+
+  if (args.help || args.h) {
+    cmd = "help";
+  }
+
+  console.log(
+    yellow("\n" + "💵 💵 💵 Welcome to the coinPriceCLI 💵 💵 💵" + "\n")
+  );
 
   switch (cmd) {
-    case "Bitcoin":
-      require("./cmd/Bitcoin")(args);
+    case "type":
+      require("./cmd/type")(args);
       break;
+
+    case "version":
+      require("./cmd/version")(args);
+      break;
+
+    case "help":
+      require("./cmd/help")(args);
+      break;
+
     default:
       console.error(red(`"${cmd}" is not a valid command!`));
       break;
