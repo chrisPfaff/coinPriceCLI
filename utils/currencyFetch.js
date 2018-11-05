96e4caea46b702f0a8322cba67cd5b88f16facf1
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-04 19:20:36 -0600

    axios get functionworking with hardcoded id

--- utils/currencyFetch.js
+++ utils/currencyFetch.js
@@ -1,12 +1,14 @@
 const axios = require("axios");
 
 module.exports = async id => {
-  const results = await axios({
-    method: "get",
-    url: `https://api.coinmarketcap.com/v2/ticker/${id}/?structure=array`,
-    params: {
-      format: "json"
-    }
-  });
+  const results = await axios
+    .get(`https://api.coinmarketcap.com/v2/ticker/${id}/?structure=array`)
+    .then(value => {
+      //wonkey api need to refactor
+      return value.data.data[0];
+    })
+    .catch(err => {
+      return err;
+    });
   return results;
 };
